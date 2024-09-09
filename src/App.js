90aea222cf7849f602a380f1deff69d94f85ed6e
gaurav-vishwa <gaurav.vishwasanta@gmail.com>
2024-09-09 18:49:59 +0530

    Added navbar and required packages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import navbar from './components/navbar'
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold ">
+    <><h1 className="text-3xl font-bold ">
       Hello world!
     </h1>
+    <div><navbar/></div></>
   )
 }
